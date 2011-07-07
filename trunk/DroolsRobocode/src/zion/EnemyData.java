4215fcf7d21882822fb1e006d2086eee1aaff97a
filipecalegario <filipecalegario@2b478484-c00d-4ea0-d286-8a8f51f9ec88>
2011-07-07 09:08:20 +0000

    versão estável

---
+++ trunk/DroolsRobocode/src/zion/EnemyData.java
@@ -0,0 +1,23 @@
+package zion;
+
+import java.io.Serializable;
+
+import robocode.ScannedRobotEvent;
+
+public class EnemyData implements Serializable {
+
+	Point p;
+	double enemyVelocity;
+	double enemyHeading;
+	
+	
+	public EnemyData(Point p, double enemyVelocity, double enemyHeading) {
+		super();
+		this.p = p;
+		this.enemyVelocity = enemyVelocity;
+		this.enemyHeading = enemyHeading;
+	}
+
+	
+	
+}
--- trunk/DroolsRobocode/src/zion/EnemySpotted.java
+++ trunk/DroolsRobocode/src/zion/EnemySpotted.java
@@ -5,6 +5,15 @@ import java.io.Serializable;
 public class EnemySpotted implements Serializable{
 
 	private String enemyName;
+	private double x;
+	private double y;
+	
+	
+	public EnemySpotted(String enemyName, double x, double y) {
+		this.enemyName = enemyName;
+		this.x = x;
+		this.y = y;
+	}
 
 	public String getEnemyName() {
 		return enemyName;
@@ -14,11 +23,31 @@ public class EnemySpotted implements Serializable{
 		this.enemyName = enemyName;
 	}
 
-	public EnemySpotted(String enemyName) {
-		super();
-		this.enemyName = enemyName;
+	public double getX() {
+		return x;
 	}
-	
-	
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public static Point calcularPonto(double x, double y, double heading, double enemyBearing, double enemyDistance){
+		Point p = null;
+		// Calculate enemy bearing
+		double bearing = heading + enemyBearing;
+		// Calculate enemy's position
+		double enemyX = x + enemyDistance * Math.sin(Math.toRadians(bearing));
+		double enemyY = y + enemyDistance * Math.cos(Math.toRadians(bearing));
+		p = new Point(enemyX, enemyY);
+		return p;
+	}
 	
 }
