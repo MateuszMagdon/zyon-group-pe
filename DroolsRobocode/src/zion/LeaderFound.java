8fb29586730b24d05329ef751cbad53a57db35ed
filipecalegario <filipecalegario@2b478484-c00d-4ea0-d286-8a8f51f9ec88>
2011-07-11 12:28:36 +0000

    antes de entregar o projeto.

---
+++ DroolsRobocode/src/zion/LeaderFound.java
@@ -0,0 +1,22 @@
+package zion;
+
+public class LeaderFound {
+	
+	private String name;
+
+	public LeaderFound(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
